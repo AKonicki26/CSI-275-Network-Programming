cc8701e8c066d12990781e9a36208ddb84ba81d2
Akonicki26 <anne.konicki@mymail.champlain.edu>
2024-01-18 22:04:12 -0500

    Finish Assignment 1

--- Week 1/main.py
+++ Week 1/main.py
@@ -1,18 +1,28 @@
 def build_list() -> []:
-    input = str('0')
-    list = []
+    input_list = []
     print("Input things until you don't feel like it anymore, type 'Done' to stop")
     while True:
         user_input = input('Enter thing: ')
-        if user_input == "done":
+        if user_input.lower() == "done":
             break
-        list.append(user_input)
-    return list
+        try:
+            user_input = float(user_input)
+            if int(user_input) == user_input:
+                user_input = int(user_input)
+            input_list.append(user_input)
+        except ValueError as ex:
+            print(f"Your inputs must be numbers!\n{type(ex).__name__}: {ex}")
+        except Exception as ex:
+            print(f"Something unexpected happened:\n{type(ex).__name__}: {ex}")
+        # exception type from https://stackoverflow.com/questions/9823936/how-do-i-determine-what-type-of-exception-occurred
+
+    return input_list
 
 
-def sort_list(list: []):
-    print(list.sort())
+def sort_list(unsorted_list: []):
+    print(sorted(unsorted_list))
+
 
 if __name__ == "__main__":
-    list = build_list()
-    sort_list(list)
+    unsorted_list = build_list()
+    sort_list(unsorted_list)
