4faef2cd6cd9f126e1cbe6f1f17c2965219ea2a6
Anne <112440960+AKonicki26@users.noreply.github.com>
2024-02-08 12:29:38 -0500

    Almost finish assignment 4

--- Week 4/Lab 4/udp_client.py
+++ Week 4/Lab 4/udp_client.py
@@ -7,6 +7,31 @@ The following code was written by Joshua Auerbach (jauerbach@champlain.edu)
 import socket
 import constants
 
+class UDPClient():
+    def __init__(self, host: str, port: int, include_id: bool = False):
+        self.host = host
+        self.port = port
+        self.include_id = include_id
+
+    def send_message_by_character(self, message: str) -> str:
+        udp_conn = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+        wait_time = constants.INITIAL_TIMEOUT
+        udp_conn.settimeout(wait_time)
+        while True: # While loop taken from slides, modified by Anne Konicki
+            try:
+                udp_conn.sendto(message.encode('ascii'), (self.host, self.port))
+                response, address = udp_conn.recvfrom(constants.MAX_BYTES)
+                return response.decode()
+            except socket.timeout:
+                print("Connection timed out")
+                wait_time *= 2
+                if wait_time >= constants.MAX_TIMEOUT:
+                    print("Exceeded max wait time")
+                    break
+                udp_conn.settimeout(wait_time)
+                # If we exceed our max wait time,
+                # break the loop
 
 def main():
     """Run some basic tests on the required functionality.
