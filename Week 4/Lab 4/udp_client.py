e91a2a489e09b43aac3e134f046ba1bc1800633c
AKonicki26 <anne.konicki@mymail.champlain.edu>
2024-02-10 20:04:32 -0500

    Probably finish lab 4?

--- Week 4/Lab 4/udp_client.py
+++ Week 4/Lab 4/udp_client.py
@@ -6,32 +6,70 @@ The following code was written by Joshua Auerbach (jauerbach@champlain.edu)
 
 import socket
 import constants
+from random import randint
 
-class UDPClient():
+
+class UDPClient:
     def __init__(self, host: str, port: int, include_id: bool = False):
         self.host = host
         self.port = port
         self.include_id = include_id
 
-    def send_message_by_character(self, message: str) -> str:
-        udp_conn = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    def verify_response(self, message_id: str, response) -> str:
+        """Verifies the message ID from a response containing one
+        by checking the message ID against the ID in the response
 
+        Returns the message after the | character if correct, otherwise returns an empty string"""
+
+        #print("Received: " + response)
+        if "|" not in response:
+            return ""
+        sections = response.split("|")
+        if len(sections) != 2:
+            return ""
+        response_id = sections[0]
+        response_character = sections[1]
+        return response_character if message_id == response_id else ""
+
+
+    def send_character(self, character: str) -> str:
+        udp_conn = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         wait_time = constants.INITIAL_TIMEOUT
         udp_conn.settimeout(wait_time)
-        while True: # While loop taken from slides, modified by Anne Konicki
+        while wait_time <= constants.MAX_TIMEOUT:
             try:
-                udp_conn.sendto(message.encode('ascii'), (self.host, self.port))
-                response, address = udp_conn.recvfrom(constants.MAX_BYTES)
-                return response.decode()
+                # If messages should be sent with a verification
+                if self.include_id:
+                    message_id = randint(0, constants.MAX_ID - 1)
+                    #print(f"Sending: {message_id}|{character}")
+                    udp_conn.sendto(f"{message_id}|{character}".encode('ascii'),
+                                    (self.host, self.port))
+                    response, address = udp_conn.recvfrom(constants.MAX_BYTES)
+                    response = response.decode('ascii')
+
+                    verify_response = self.verify_response(message_id, response)
+                    if verify_response == "":
+                        continue
+                    return verify_response
+                # If messages do not need a verification
+                else:
+                    #print("Sending: " + character)
+                    udp_conn.sendto(character.encode('ascii'), (self.host, self.port))
+                    response, address = udp_conn.recvfrom(constants.MAX_BYTES)
+                    response = response.decode('ascii')
+                    return response
             except socket.timeout:
-                print("Connection timed out")
+                #print("Connection timed out")
                 wait_time *= 2
-                if wait_time >= constants.MAX_TIMEOUT:
-                    print("Exceeded max wait time")
-                    break
                 udp_conn.settimeout(wait_time)
-                # If we exceed our max wait time,
-                # break the loop
+        # Loop has exited, max wait time exceeded
+        #print("Exceeded maximum wait time")
+        return ""
+
+
+    def send_message_by_character(self, message: str) -> str:
+        return "".join([self.send_character(character) for character in message])
+
 
 def main():
     """Run some basic tests on the required functionality.
